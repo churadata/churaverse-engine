d28576350b5881089f69efb48c7c27c907dd4fd7
kota1206 <kota.yahagi@churadata.okinawa>
2026-02-10 18:09:59 +0900

    型定義の変更

--- churaverse-engine-client/src/scene/types.ts
+++ churaverse-engine-client/src/scene/types.ts
@@ -3,12 +3,11 @@ import { IMainScene } from './IScene/IMainScene'
 import { IMeetingScene } from './IScene/IMeetingScene'
 import { ITitleScene } from './IScene/ITitleScene'
 
-export type Scenes = ITitleScene | IMainScene | IMeetingScene
-
 export interface SceneMap {
   TitleScene: ITitleScene
   MainScene: IMainScene
   MeetingScene: IMeetingScene
 }
 
+export type Scenes = SceneMap[keyof SceneMap]
 export type SceneName = keyof SceneMap
